1577009a5bb760f33a1bf57db84c5be9871c28cd
Mikko Haapoja <me@mikkoh.com>
2015-06-01 16:00:55 -0400

    Updaed readme and added cleaner tests

--- README.md
+++ README.md
@@ -2,11 +2,40 @@
 
 [![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
 
-This module will take the d1-dom module and \"oldify\" it. This means it will remove css transforms and instead use other css methods
+This module will take the f1-dom module and "oldify" it. For instance instead of running css 3d transforms it will emulate the same through ie filter operations on older versions of ie.
 
 ## Usage
 
 [![NPM](https://nodei.co/npm/f1-dom-oldify.png)](https://www.npmjs.com/package/f1-dom-oldify)
+
+This module expects `f1-dom` to be a peer dependency so first do:
+```
+$ npm i f1-dom --save
+$ npm i f1-dom-oldify --save
+```
+
+Then in your source code you have two options.
+
+### Option #1 just require and use:
+```javascript
+var f1 = require('f1');
+var f1DomOldify = require('f1-dom-oldify');
+
+var ui = f1()
+.parsers(f1DomOldify);
+```
+The above method is similar to using `f1-dom`.
+
+### Option #2 oldify `f1-dom`:
+```javascript
+var f1 = require('f1');
+var f1Dom = require('f1-dom');
+require('f1-dom-oldify').oldify();
+
+var ui = f1()
+.parsers(f1Dom);
+```
+The above method will modify `f1-dom` to support older browsers. Most likely you'd perform a check that oldification should happen before doing it. For instance check if this is an old IE version then oldify.
 
 ## License
 
--- test/index.js
+++ test/index.js
@@ -1,156 +1,104 @@
 var f1DomOldify = require('./..');
-var test = require( 'tape' );
 var supportedProps = require( '../supportedProps' );
 
-var toApply = {
+var tests = [
+  function opacity() {
+    apply(getTestEl('opacity'), {
+      alpha: 0.5
+    });
+  },
 
-  color: [ 11, 22, 33, 0.5 ],
-  backgroundColor: [ 22, 33, 44, 0.3 ],
-  position: [ 500, 100, 200 ],
-  scale: [ 0.5, 0.63, 0.8 ],
-  rotation: [ 45, 0, 45 ],
-  perspective: 200,
-  anchor: [ 0.33, 0.8 ],
-  alpha: 0.5
-};
+  function backgroundColor() {
+    apply(getTestEl('backgroundColor'), {
+      backgroundColor: [ 255, 0, 255, 0.1 ]
+    });
+  },
 
-test( 'check supported props', function( t ) {
+  function text() {
+    apply(getTestEl('text'), {
+      text: '<div>something</div>'
+    });
+  },
 
-  t.deepEqual( supportedProps, {
+  function html() {
+    apply(getTestEl('html'), {
+      html: '<div>something</div>'
+    });
+  },
 
-    position: Array,
-    scale: Array,
-    rotation: Array,
-    perspective: Number,
-    anchor: Array,
-    alpha: Number,
-    text: String,
-    html: String,
-    color: Array,
-    backgroundColor: Array
-  }, 'supported props do not match' );
+  function test2dPos() {
+    apply(getTestEl('test2dPos'), {
+      position: [100, 100]
+    });
+  },
 
-  t.end();
+  function test3dPos() {
+    apply(getTestEl('test3dPos'), {
+      perspective: 1000,
+      position: [200, 100, -500]
+    });
+  },
+
+  function test2dRotation() {
+    apply(getTestEl('test2dRotation'), {
+      rotation: [0, 0, Math.PI * 0.1]
+    });
+  },
+
+  function test3dRotation() {
+    apply(getTestEl('test3dRotation'), {
+      rotation: [Math.PI * 0.25, Math.PI * 0.25, 0]
+    });
+  },
+
+  function testTransformOrigin() {
+    apply(getTestEl('testTransformOrigin'), {
+      anchor: [0, 0, 0],
+      rotation: [0, 0, Math.PI * 0.25]
+    });
+  },
+
+  function testScale() {
+    apply(getTestEl('testScale'), {
+      scale: [0.5, 0.1, 1]
+    });
+  },
+
+  function testCombined() {
+    apply(getTestEl('testCombined'), {
+      perspective: 1000,
+      position: [100, 50, -900],
+      rotation: [Math.PI * 0.1, Math.PI * 0.1, 0]
+    });
+  }
+];
+
+tests.forEach(function(test) {
+  test();
 });
 
-test( 'apply transform', function( t ) {
 
-  var el = getEL();
-
-  applyParsers( el, toApply );
-
-  document.body.appendChild( el );
-
-  t.equal( el.style.transform, 
-          'matrix3d(0.262660980224609, 0.281608939170837, 0.579229474067688, 0, -0.425451755523682, 0.173856809735298, 0.357598662376404, 0, 0, -0.536069214344025, 0.420257598161697, -0.0500000007450581, 500, 100, 200, 1)',
-          'transform correct' );
-
-  t.end();
-});
-
-test( 'apply transform orgin', function( t ) {
-
-  var el = getEL();
-
-  applyParsers( el, {
-    anchor: [0.33, 0.8, 0]
-  });
-
-  document.body.appendChild( el );
-
-  t.equal( el.style[ 'transform-origin' ], '33% 80% 0px', 'transform origin correct' );
-
-  t.end();
-});
-
-test( 'apply opacity', function( t ) {
-
-  var el = getEL();
-
-  applyParsers( el, {
-    alpha: 0.5
-  });
-
-  document.body.appendChild( el );
-
-  t.equal( el.style.opacity, '0.5', 'transform origin correct' );
-
-  t.end();
-});
-
-test( 'apply html', function( t ) {
-
-  var el = getEL();
-
-  applyParsers( el, { html: '<div>snakey snake</div>' } );
-
-  t.equal( el.innerHTML, '<div>snakey snake</div>', 'html set correctly' );
-
-  t.end();
-});
-
-test( 'apply text', function( t ) {
-
-  var el = getEL();
-
-  applyParsers( el, { text: '<div>snakey snake</div>' } );
-
-  t.equal( el.innerHTML, '&lt;div&gt;snakey snake&lt;/div&gt;', 'text set correctly' );
-
-  t.end();
-});
-
-test( 'apply color 4', function( t ) {
-
-  var el = getEL();
-  el.innerHTML = 'apply color 4';
-
-  el.innerHTML = 'i should have colour set';
-  applyParsers( el, {
-    color: [ 255, 255, 255, 0.5 ]
-  });
-
-  applyParsers( el, {
-    backgroundColor: [ 255, 0, 255, 0.3 ]
-  });
-
-  t.equal( el.style.color, 'rgba(255,255,255,0.5)', 'color set correctly' );
-  t.equal( el.style.backgroundColor, 'rgba(255,0,255,0.3)', 'background-color set correctly' );
-
-  t.end();
-});
-
-function applyParsers(el, toApply) {
-  f1DomOldify.forEach( function(parser) {
-    parser(el, toApply);
+function apply(el, testData) {
+  f1DomOldify.forEach( function(func) {
+    func(el, testData);
   });
 }
 
-function getEL() {
+function getTestEl(name) {
 
-  var el;
-  var container;
+  var el = document.createElement('div');
+  var parent = document.createElement('div');
 
-  try {
-      container = document.createElement( 'div' );
+  el.style.background = '#FF0000';
+  el.style.width = el.style.height = '100px';
 
-      el = document.createElement( 'div' );
+  parent.style.height = '200px';
+  parent.style.position = 'relative';
+  parent.style.border = '1px solid #000';
+  parent.innerHTML = name;
 
-      el.style.backgroundColor = '#CAFE00';
-      el.style.width = el.style.height = '100px';
-      el.style.position = 'absolute';
-      el.style.left = el.style.top = '0px';
-
-      container.style.position = 'relative';
-      container.style.height = '200px';
-
-      container.appendChild(el);
-      document.body.appendChild(container);
-  } catch(e) {
-
-    el = {};
-    el.style = {};
-  }
+  document.body.appendChild(parent);
+  parent.appendChild(el);
 
   return el;
 }
